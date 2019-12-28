91ae3cac16d329196f88be01c9d254064b0fe6c2
Judy Chiang <jiyc1119@gmail.com>
2019-12-28 15:24:52 -0800

    WIP Level 2 sort obj by field

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,1 +1,15 @@
-answer.js
+function sortField(obj, field){
+
+
+}
+
+
+
+
+var input =
+  [
+    { name: 'John', age: 42, gender: 'M' },
+    { name: 'Jack', age: 34, gender: 'M' },
+    { name: 'Michael', age: 40, gender: 'M' },
+    { name: 'Kendra', age: 48, gender: 'F' }
+  ];
