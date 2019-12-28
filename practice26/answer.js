494a58082e4d71ada35a5e578444f0a1bad06122
Judy Chiang <jiyc1119@gmail.com>
2019-12-28 15:53:50 -0800

    completed search array

--- practice26/answer.js
+++ practice26/answer.js
@@ -1,1 +1,17 @@
-answer.js
+var search = function (arr1, arr2) {
+  var result = [];
+  for(var i =0 ; i <arr1.length; i++){
+    for(var k =0; k<arr2.length; k++){
+      if(arr1[i] === arr2[k]){
+        result.push(arr1[i]);
+      }
+    }
+  }
+  return result;
+};
+
+
+var haystack = ['cat', 'dog', 'bird', 'turtle', 'lizard'];
+var needle = ['dog', 'lizard', 'flower', 'monkey', 'unicorn'];
+
+console.log(search(needle, haystack)); //['dog','lizard']
