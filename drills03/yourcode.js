0957991cc73a882011bd4e2ed1b82bc55c508e7f
Judy Chiang <jiyc1119@gmail.com>
2019-12-31 01:19:17 -0800

    wip getPathParts;  completed getPath, getCapitalCount, correctCalcChecker

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,21 +1,60 @@
 
 
-function getPath(){
+function getPath(str) {
+  return str.split("/");
 
 }
-
-function getPathParts(){
-
+//hardcoded
+function getPathParts(str) {
+  str = str.split(":");
+  var result = {};
+  result.protocol = str[0];
+  result.host = str[1].slice(2);
+  result.port = parseInt(str[2].slice(0, 4));
+  result.path = str[2].slice(4, 19);
+  result.file = str[2].slice(-9);
+  return result;
 }
 
-function getCapitalCount(){
-
+function getCapitalCount(arr) {
+  var num = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][0] === arr[i][0].toUpperCase()) {
+      num++;
+    }
+  }
+  return num;
 }
 
-function correctCalcChecker(){
-
+function correctCalcChecker(arr) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    var math = arr[i];
+    if (math.op === '/') {
+      if (math.num1 / math.num2 === math.result) {
+        result.push(arr[i])
+      }
+    }
+    if (math.op === '-') {
+      if (math.num1 - math.num2 === math.result) {
+        result.push(arr[i])
+      }
+    }
+    if (math.op === '+') {
+      if (math.num1 + math.num2 === math.result) {
+        result.push(arr[i])
+      }
+    }
+    if (math.op === '*') {
+      if (math.num1 * math.num2 === math.result) {
+        result.push(arr[i])
+      }
+    }
+  }
+  return result;
 }
 
-function doMath(){
+// function doMath(){
 
-}
+// }
