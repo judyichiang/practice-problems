42e9251896d789818cad91124505d0e685742391
Judy Chiang <jiyc1119@gmail.com>
2020-01-01 21:21:25 -0800

    hardcoded - refer to Pract19 sort object

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,10 +1,3 @@
-function sortField(obj, field){
-
-
-}
-
-
-
 
 var input =
   [
@@ -13,3 +6,25 @@ var input =
     { name: 'Michael', age: 40, gender: 'M' },
     { name: 'Kendra', age: 48, gender: 'F' }
   ];
+
+input.sort(function (a, b) {
+  return a.age - b.age;
+});
+console.log(input);
+
+
+// var input =
+//   [
+//     { name: 'John', age: 42, gender: 'M' },
+//     { name: 'Jack', age: 34, gender: 'M' },
+//     { name: 'Michael', age: 40, gender: 'M' },
+//     { name: 'Kendra', age: 48, gender: 'F' }
+//   ];
+
+// function sortObj(input, field) {
+//   input.sort(function (a, b) {
+//     return a.field - b.field;
+//   });
+// }
+
+// console.log(sortObj(input, 'age'));
